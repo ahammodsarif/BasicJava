7c18e4e63e26ab94243726e97abfaa06c03d0ed7
Ahammod Sarif <ahammodtannababu@gmail.com>
2024-10-12 16:04:09 +0600

    first commit

---
+++ src/basicjava/BioData.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basicjava;
+
+/**
+ *
+ * @author MIKE
+ */
+public class BioData {
+    
+    public static void main(String[]args){
+        
+        System.out.println("Hello World!");
+        System.out.println("My name is Ahmed Tanna.");
+        System.out.println("I'm a Student.");
+        System.out.println("Green University of Bangladesh.");
+    }
+    
+}
